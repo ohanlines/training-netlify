bf504e091344266545c2806d45c2a6f3e26615d1
Ahmad Rauhan <rauhanhouse7@gmail.com>
2023-01-09 11:24:59 +0700

    try identity-login with ts

---
+++ netlify/functions/identity-login.ts
@@ -0,0 +1,15 @@
+import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+
+const handler: Handler = async function (event: HandlerEvent, context: HandlerContext) {
+    const { identity, user } = context.clientContext;
+    console.log('logging');
+    console.log(identity);
+    console.log(user);
+    // Do stuff and return a response...
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ url: user.url, email: identity.email }),
+    };
+};
+
+export { handler };
